1874739aab572aeb199b8e5abd6cedc9bf70916f
Oliver Pollard <oliver.pollard1@gmail.com>
2024-01-29 15:28:54 +0000

    Updated for line geometry

--- shapy/main.py
+++ shapy/main.py
@@ -1,9 +1,10 @@
 import numpy as np
 from scipy.interpolate import interp1d
-from shapely.geometry import Point, Polygon
+from shapely.geometry import Point, Polygon, LineString
+from scipy.spatial.distance import euclidean
 
 
-def calc_polygon_dist(x, y):
+def calc_cumulative_dist(x, y):
     dist = [0]
     for i in range(len(x) - 1):
         dist.append(np.sqrt((x[i + 1] - x[i]) ** 2 + (y[i + 1] - y[i]) ** 2))
@@ -11,17 +12,124 @@ def calc_polygon_dist(x, y):
     return dist
 
 
-def points_to_polygon(x, y):
+def mapping_to_mid(coords_1, coords_2, mapping):
+    mids = []
+    for key, val in mapping.items():
+        p1 = np.array(coords_1)[key]
+        p2 = np.array(coords_2)[val]
+        mids.append((p1 + p2) / 2)
+    return np.asarray(mids)
+
+
+def relative_mapping(geom_from, geom_to):
+    mapping = {idx: idx for idx in range(len(geom_from.coords))}
+    return geom_from, geom_to, mapping
+
+
+def nearest_mapping(ls_1, ls_2):
+    mapping, poly = calc_line_mapping(
+        interior_polygon=ls_2, exterior_polygon=ls_1, one_to_one=True
+    )
+    x, y = poly.exterior.xy
+    ls_1 = LineString(list(zip(x[0:-1], y[0:-1])))
+
+    return ls_1, ls_2, mapping
+
+
+def xy_to_geom(x, y, geom_type):
     points = [[x[i], y[i]] for i in range(len(x))]
-    return Polygon(points)
+    return geom_type(points)
 
 
-class PolygonSampler:
-    def __init__(self, x, y):
+def coords_to_geom(coords, geom_type):
+    points = [[coords[i, 0], coords[i, 1]] for i in range(len(coords))]
+    return geom_type(points)
+
+
+def geom_to_coords(geom):
+    if isinstance(geom, LineString):
+        coords = geom.coords
+    elif isinstance(geom, Polygon):
+        coords = geom.exterior.coords
+    else:
+        raise NotImplementedError
+
+    coords = np.asarray(coords)
+    return coords
+
+
+def geom_to_xy(geom):
+    if isinstance(geom, LineString):
+        x, y = geom.coords.xy
+    elif isinstance(geom, Polygon):
+        x, y = geom.exterior.coords.xy
+    else:
+        raise NotImplementedError
+
+    x, y = list(x), list(y)
+    return x, y
+
+
+class ShapeMapping:
+    def __init__(self, geom_from, geom_to, mapping, geom_type=None):
+        self.geom_from = geom_from
+        self.geom_to = geom_to
+        self.mapping = mapping
+        self.geom_type = geom_type
+
+    def vector_dists(self):
+        dists = []
+        for key, val in self.mapping.items():
+            point_from = self.coords_from[key]
+            point_to = self.coords_to[val]
+            dists.append(euclidean(point_from, point_to))
+        return np.asarray(dists)
+
+    @property
+    def xy_from(self):
+        return geom_to_xy(geom=self.geom_from)
+
+    @property
+    def xy_to(self):
+        return geom_to_xy(geom=self.geom_to)
+
+    @property
+    def coords_from(self):
+        return geom_to_coords(geom=self.geom_from)
+
+    @property
+    def coords_to(self):
+        return geom_to_coords(geom=self.geom_to)
+
+
+class LineMapping(ShapeMapping):
+    def __init__(self, geom_from, geom_to, mapping):
+        super().__init__(
+            geom_from=geom_from, geom_to=geom_to, mapping=mapping, geom_type=LineString
+        )
+
+    def interp(self, d):
+        coords_from_mapped = self.coords_from[list(self.mapping.keys())]
+        coords_to_mapped = self.coords_to[list(self.mapping.values())]
+
+        coords_interp = coords_from_mapped + (
+            (coords_to_mapped - coords_from_mapped) * d
+        )
+
+        return coords_to_geom(coords_interp, geom_type=self.geom_type)
+
+
+class PolygonMapping(ShapeMapping):
+    pass
+
+
+class ShapeSampler:
+    def __init__(self, x, y, geom_type=None):
         self.x, self.y = x, y
-        self.dist = calc_polygon_dist(x=x, y=y)
-        self.interp_x = interp1d(x=self.dist, y=x)
-        self.interp_y = interp1d(x=self.dist, y=y)
+        self.cumulative_dist = calc_cumulative_dist(x=x, y=y)
+        self.interp_x = interp1d(x=self.cumulative_dist, y=x)
+        self.interp_y = interp1d(x=self.cumulative_dist, y=y)
+        self.geom_type = geom_type
 
     def sample_dist(self, d, coords=False):
         x_sample = self.interp_x(d)
@@ -33,7 +141,7 @@ class PolygonSampler:
             return Point([x_sample, y_sample])
 
     def sample_n(self, n, coords=False):
-        d = np.linspace(0, self.margin_length, n)
+        d = np.linspace(0, self.length, n)
 
         x_sample = self.interp_x(d)
         y_sample = self.interp_y(d)
@@ -41,21 +149,50 @@ class PolygonSampler:
         if coords is True:
             return x_sample, y_sample
         else:
-            return points_to_polygon(x=x_sample, y=y_sample)
+            return xy_to_geom(x=x_sample, y=y_sample, geom_type=self.geom_type)
 
-    @property
-    def margin_length(self):
-        return self.dist[-1]
+    def map_to(self, sampler_to, n, mapping_func=relative_mapping):
+        geom_to = sampler_to.sample_n(n)
+        geom_from, geom_to, mapping = mapping_func(
+            geom_from=self.sample_n(n), geom_to=geom_to
+        )
+        return mappings[self.geom_type](
+            geom_from=geom_from, geom_to=geom_to, mapping=mapping
+        )
 
     @property
     def geometry(self):
-        return points_to_polygon(x=self.x, y=self.y)
+        return xy_to_geom(x=self.x, y=self.y, geom_type=self.geom_type)
+
+    @property
+    def length(self):
+        return self.cumulative_dist[-1]
+
+
+class LineSampler(ShapeSampler):
+    def __init__(self, x, y):
+        super().__init__(x=x, y=y, geom_type=LineString)
+
+    @classmethod
+    def from_line(cls, line):
+        x, y = line.coords.xy
+        x, y = list(x), list(y)
+        return cls(x=x, y=y)
+
+
+class PolygonSampler(ShapeSampler):
+    def __init__(self, x, y):
+        super().__init__(x=x, y=y, geom_type=Polygon)
 
     @classmethod
     def from_polygon(cls, polygon):
         x, y = polygon.exterior.coords.xy
         x, y = list(x), list(y)
-        return cls(x, y)
+        return cls(x=x, y=y)
+
+
+samplers = {Polygon: PolygonSampler, LineString: LineSampler}
+mappings = {Polygon: PolygonMapping, LineString: LineMapping}
 
 
 def dict_keys_to_int(dict):
